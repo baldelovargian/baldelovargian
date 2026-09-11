51cd9bbd30cf6cfa0b3c76f2a4b58f88e7d6cb8c
baldelovargian <c20245092.baldelovar@csav.edu.ph>
2026-09-11 09:20:16 +0800

    varibles (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+//create a const variable name myname
+const myname = `Gian A Baldelovar`;
+let age = 18;
+let number = 9158764715;
+const address = `hda la consolacion`; 
+
+console.log(`Name: ${myname}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
